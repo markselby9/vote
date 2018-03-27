0ac4dc49ec79957f32bb8275176745810afce404
Mark Feng <markselbyfcy@gmail.com>
2018-03-27 19:31:52 +0800

    import web3 library

---
+++ vote-web/src/components/Vote.vue
@@ -0,0 +1,21 @@
+<template>
+  <section>
+    <p>vote</p>
+  </section>
+</template>
+
+<script>
+import web3 from '../lib/web3';
+
+export default {
+  name: 'Vote',
+  created() {
+    console.log(web3.eth.accounts.wallet);
+  },
+  data() {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+    };
+  },
+};
+</script>
